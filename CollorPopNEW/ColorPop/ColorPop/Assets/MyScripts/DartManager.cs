ae5458165f072c8470a961cbf9a6fbcb643d2d3d
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-09-25 11:15:38 -0400

    newest

---
+++ CollorPopNEW/ColorPop/ColorPop/Assets/MyScripts/DartManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartManager : MonoBehaviour {
+	private int darts;
+    private int kDarts;
+    public SetGrid redoGrid;
+	public ChooseGameScript chooseGameScript;
+	// Use this for initialization
+	void Start () {
+		darts = 0;
+
+	}
+	public void SetDarts(int amountOfDarts)
+	{
+		darts = amountOfDarts;
+        kDarts = amountOfDarts;
+	}
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (darts);
+	}
+	public void SubtractDart()
+	{
+		darts--;
+	}
+	public int GetDarts(){
+		return darts;
+	}
+    public void ResetDarts()
+    {
+        redoGrid.BuildGrid();
+        darts = kDarts;
+    }
+}
---
+++ CollorPopNEW/ColorPop/ColorPop/Assets/MyScripts/TicketManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class TicketManager : MonoBehaviour {
+	private int tickets;
+    private int kTix;
+    public GameObject loseScreen;
+	public List<Button> tix = new List<Button>();
+
+	public ChooseGameScript gameManager;
+	// Use this for initialization
+	void Start () {
+		tickets = 0;
+        kTix = 0;
+	}
+	void Update()
+	{
+		CheckTickets();
+	}
+	private void CheckTickets()
+	{
+		for(int i = 0; i < this.GetTix(); i++)
+		{
+			tix[i].interactable = true;
+		}
+	}
+	public void SetTix(int tix)
+	{
+		tickets = tix;
+        kTix = tix;
+	}
+	public int GetTix()
+	{
+		return tickets;
+	}
+    public int GetInitTix()
+    {
+        return kTix;
+    }
+    public void OutOfTicketsLOSE()
+    {
+        StartCoroutine(Lose());
+    }
+
+    IEnumerator Lose()
+    {
+        yield return new WaitForSeconds(1.5f);
+        loseScreen.SetActive(true);
+        yield return 0;
+    }
+}
