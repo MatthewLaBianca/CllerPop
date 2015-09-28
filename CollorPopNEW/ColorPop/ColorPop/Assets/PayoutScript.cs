bf8340f19c37308404771d108e2a53e7008859c3
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-09-28 15:53:35 -0400

    payout chart added

---
+++ CollorPopNEW/ColorPop/ColorPop/Assets/PayoutScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PayoutScript : MonoBehaviour {
+    public GameObject payoutPanal;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TogglePayoutPanal()
+    {
+        if(!payoutPanal.activeSelf)
+        {
+            payoutPanal.SetActive(true);
+        }
+        else
+        {
+            payoutPanal.SetActive(false);
+        }
+    }
+
+
+}
