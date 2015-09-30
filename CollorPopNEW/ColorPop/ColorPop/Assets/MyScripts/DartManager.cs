cc666d875b5d536d04c17ce31855e1de50c2d27b
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-09-30 13:19:14 -0400

    keeps track of darts

--- CollorPopNEW/ColorPop/ColorPop/Assets/MyScripts/DartManager.cs
+++ CollorPopNEW/ColorPop/ColorPop/Assets/MyScripts/DartManager.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class DartManager : MonoBehaviour {
 	private int darts;
     private int kDarts;
     public SetGrid redoGrid;
 	public ChooseGameScript chooseGameScript;
+
+    public List<Button> dartsUI = new List<Button>();
+    private List<Button> activeDart = new List<Button>();
 	// Use this for initialization
 	void Start () {
 		darts = 0;
-
+        CheckDarts();
 	}
 	public void SetDarts(int amountOfDarts)
 	{
@@ -18,11 +22,35 @@ public class DartManager : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log (darts);
+        //Debug.Log (darts);
+        //CheckDarts();
+        //CheckActiveDarts();
 	}
-	public void SubtractDart()
+
+    public void CheckDarts()
+    {
+        foreach(Button dart in dartsUI)
+        {
+            dart.interactable = false;
+        }
+        for (int i = 0; i < this.GetDarts(); i++)
+        {
+            dartsUI[i].interactable = true;
+            //activeDart.Add(dartsUI[i]);
+        }
+    }
+
+    private void CheckActiveDarts()
+    {
+        foreach(Button dart in activeDart)
+        {
+            dart.interactable = true;
+        }
+    }
+    public void SubtractDart()
 	{
 		darts--;
+        CheckDarts();
 	}
 	public int GetDarts(){
 		return darts;
@@ -35,6 +63,7 @@ public class DartManager : MonoBehaviour {
     {
         redoGrid.BuildGrid();
         darts = kDarts;
+        CheckDarts();
     }
     public void ReplayDarts()
     {
